09e8359b6ffe53d59b30baa8a1690f1d40bf7386
golez <45137041+vanillaMech@users.noreply.github.com>
2021-01-20 08:37:53 -0800

    Add files via upload

---
+++ Forum/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORUM</title>
+</head>
+<body>
+
+    <center>
+        <h1>WELCOME</h1>
+    </center>
+
+
+</body>
+</html>
---
+++ Forum/login.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+    <center>
+    <h1>Login</h1>
+        <form>
+            <input type="text" value="" placeholder="Username"><br>
+            <input type="text" value="" placeholder="Password"><br><br>
+            <input type="submit" value="Login">
+        </form>
+
+    <br><br>
+    <h1>Register</h1>
+        <form>
+            <input type="text" value="" placeholder="Firstname"><br>
+            <input type="text" value="" placeholder="Lastname"><br>
+            <input type="text" value="" placeholder="Username"><br>
+            <input type="text" value="" placeholder="Password"><br><br>
+            <input type="submit" value="Register">
+        </form>
+    </center>
+</body>
+</html>
