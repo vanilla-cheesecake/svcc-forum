0db0c1958d3aef25a5633513772509f0ebc48b9c
golez <golez.sf@gmail.com>
2021-01-23 03:26:51 +0800

    Updated reg_proccess.php
    Fixed
    - Only one email and username can register

--- Forum/php/reg_process.php
+++ Forum/php/reg_process.php
@@ -18,27 +18,39 @@ if(isset($_POST['register'])){
     // Errors: forms need to be filled properly
     if(empty($firstname)){array_push($errors, "First name is required");}
     if(empty($lastname)){array_push($errors, "Lastname is required");}
-    if(empty($email)){array_push($errors, "Email is required");}
-    if(empty($username)){array_push($errors, "Username is required");}
-    if(empty($password)){array_push($errors, "Password is required");}
+    if(empty($email)){array_push($errors, "Invalid email");}
+    if(empty($username)){array_push($errors, "Invalid username");}
+    if(empty($password)){array_push($errors, "Invalid password");}
     // If there are no errors: All field are filled
     if(count($errors) == 0){
+        
+        $user_email = mysqli_query($conn, "SELECT email FROM users where email = '$email' ");
+        $user_username = mysqli_query($conn, "SELECT username FROM users where username = '$username' ");
+        $result1 = mysqli_num_rows($user_email);
+        $result2 = mysqli_num_rows($user_username);
+        
+        if($result1 > 0){
+            array_push($errors, "Email is already been registered");
+        }elseif($result2 > 0){
+            array_push($errors, "Username has already been taken please choose another");
+        } else {
         // INSERT DATA INTO USER TABLE
         $insert = $conn->query("INSERT INTO users(`name`, `lastname`, `email`, `username`, `password`) VALUES 
-                                ('$firstname', '$lastname', '$email', '$username', '$password')");    
-    // DATA QUERY
-    // $select = $conn->query("SELECT * FROM users WHERE name = '$firstname' AND lastname = '$lastname' AND username = '$username' AND password = '$password'");
-    // $count = $select->num_rows;
-    // $row = $select->fetch_assoc();
-    $_SESSION['firstname'] = $firstname;
-    $_SESSION['lastname'] = $lastname;
-    $_SESSION['email'] = $email;
-    $_SESSION['username'] = $username;
-    $_SESSION['password'] = $password;
-    $_SESSION['success'] = "You are now logged in"; 
-    // After successfull registration direct into the homepage
-    header('Location: index.php');
-    }
+                                    ('$firstname', '$lastname', '$email', '$username', '$password')");    
+        // DATA QUERY
+        // $select = $conn->query("SELECT * FROM users WHERE name = '$firstname' AND lastname = '$lastname' AND username = '$username' AND password = '$password'");
+        // $count = $select->num_rows;
+        // $row = $select->fetch_assoc();
+
+        $_SESSION['firstname'] = $firstname;
+        $_SESSION['lastname'] = $lastname;
+        $_SESSION['email'] = $email;
+        $_SESSION['username'] = $username;
+        $_SESSION['password'] = $password;
+        $_SESSION['success'] = "You are now logged in"; 
+        // After successfull registration direct into the homepage
+        header('Location: index.php');
+        }}
 }
 // LOGIN 
 if(isset($_POST['submit'])){
@@ -57,7 +69,7 @@ if(isset($_POST['submit'])){
         // entered username exists 
         $count = $select->num_rows;
         $row = $select->fetch_assoc();
-            if($count == 1){
+        if($count == 1){
                 // Storing username in session variable 
                 $_SESSION['name'] = $row['name'];
                 $_SESSION['lastname'] = $row['lastname'];
