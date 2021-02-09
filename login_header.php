2bfc06966778fa848eb3e4033dbab27fab320eb9
golez <golez.sf@gmail.com>
2021-02-10 02:44:00 +0800

    Separating changes to new branch
    making experimental changes

--- login_header.php
+++ login_header.php
@@ -48,9 +48,9 @@
       <!---End of Breadcrumb Navbar-->
       <section class="main">
         <!--img------------------->
-        <div class="main-img">
+        <!-- <div class="main-img">
             <img src="image/wizard.jpeg"/>
-        </div>
+        </div> -->
         <!--text-and-btns--------------->
         <div class="text-btns">
             <p>Hello, welcome to SVCC Forum .</p>
@@ -61,13 +61,13 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script crossorigin="anonymous" src="https://kit.fontawesome.com/c8e4d183c2.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-  
-
+   -->
+<!-- 
 <script>
     $(document).on('click','.login-btn,.sign-up-btn',function(){
         $('.main').addClass('main-active')
@@ -91,7 +91,7 @@
         $('.side-sign-up').removeClass('side-sign-up-active')
     });
  </script>
- </section>
+ </section> -->
 </body>
 </html>
 
